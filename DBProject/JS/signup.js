abbf1501f3929171a962b64fdbbc1c155ce1da19
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2020-12-23 22:18:04 +0200

    user

---
+++ DBProject/JS/signup.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+   
+    $("#name").on("focusout",function () {
+        if($(this).val().length==0)
+        {
+            $(this).css("border-color","red");
+           $("#h_name").append("This is empty");
+           
+        }
+        else{
+            $("#h_name").remove();
+            $(this).css("border-color","white");
+        }
+    }
+    );
+    $("#email").on("focusout",function () {
+        
+        if($(this).val().length==0)
+        {
+           $("#h_email").append("This is empty");
+           $(this).css("border-color","red");
+        }
+        else{
+            $("#h_email").remove();
+            $(this).css("border-color","white");
+        }
+    })
+    $("#number").on("focusout",function () {
+        
+        if($(this).val().length==0)
+        {
+            $(this).css("border-color","red");
+           $("#h_number").append("This is empty");
+        }
+        else{
+            $("#h_number").remove();
+            $(this).css("border-color","white");
+        }
+    })
+    $("#pass1").on("focusout",function () {
+        
+        if($(this).val().length==0)
+        {
+            $(this).css("border-color","red");
+           $("#h_pass1").append("This is empty");
+        }
+        else{
+            $("#h_pass1").remove();
+            $(this).css("border-color","white");
+        }
+    })
+    $("#pass2").on("focusout",function () {
+        
+        if($(this).val().length==0)
+        {
+            $(this).css("border-color","red");
+           $("#h_pass2").append("This is empty");
+        }
+        else{
+            $("#h_pass2").remove();
+            $(this).css("border-color","white");
+        }
+    })
+   
+    
+})
