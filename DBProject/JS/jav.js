9c02dec777b4f132f309c4f344f455e7f5282306
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2020-12-24 15:56:05 +0200

    Group

---
+++ DBProject/JS/jav.js
@@ -0,0 +1,13 @@
+function display()
+{
+    const x=document.getElementsByClassName("cla10")[0];
+ console.log(x);
+ x.style.display="block";
+
+}
+const body=document.getElementsByTagName("body")[0];
+function hide()
+{
+    const x=document.getElementsByClassName("cla10")[0];
+    x.style.display="none";
+}
